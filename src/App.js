a5623608a04a7b58ca5ba7a0565aed21710ad4c5
Matheus Santos <matheus.santos@rotaexata.com.br>
2021-08-23 12:26:45 -0300

    Removing unusable code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
-import Footer from './Components/Footer';
 
 import './App.css';
 
 // Routes
 import Home from './Pages/Home';
+import Details from './Pages/Details';
 
 function App() {  
   return (
@@ -15,9 +15,8 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/details/:name" element={<Details />} />
         </Routes>
-
-        <Footer />
     </BrowserRouter>
   )
 }
