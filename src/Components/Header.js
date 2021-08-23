d4603161f5e83e684c4d18ace3eb7aedb737e084
Matheus Santos <matheus.santos@rotaexata.com.br>
2021-08-23 16:15:51 -0300

    Redirecting to home when clicking on snorlax

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import './Header.css';
 
 const Header = () => {
+    const navigate = useNavigate();
+    
+    function goHome() {
+        navigate('/');
+    }
+
     return (
         <header>
-            <div className="logo-content">
+            <div className="logo-content" onClick={goHome}>
                 <img src="/snorlax_64.png" alt="Snorlax" />
                 
                 <h3>Pokedex</h3>
