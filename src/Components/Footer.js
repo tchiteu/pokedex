bf21eab35b676e14e78b43be8afefcc959ef004a
Matheus Santos <matheus.santos@rotaexata.com.br>
2021-08-23 01:28:44 -0300

    Creating Footer component

---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            <a target="_blank" href="https://github.com/tchiteu">Author: Matheus Santos</a>
+        </footer>
+    );
+}
+
+export default Footer;
